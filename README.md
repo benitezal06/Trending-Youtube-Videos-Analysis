6e775b9336e63b3bd52912a7753e58e5b9b4d91e
Bustamante <Familia@Dianas-MacBook-Pro-3.local>
2020-06-24 16:38:01 -0700

    added README & edited html

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# Trending-Youtube-Videos_Analysis
-Bubble-charts; Trending-Youtube-Videos_Analysis
+# Trending-Youtube-Videos_Analysis and Category predictor
+Tools used: 
 
-The csv files "USvideos.csv" and "CAvideos.csv" in the "youtubecsv" folder were to big to upload to github. Below are links to both csv files to be downloaded into the youtubecsv folder . Feel free to reach out to me for questions or concerns. 
-
+Python: pandas, pymongo, flask, Naive Bayes model("probabilistic classifier"), spark (work in progress): tokenizer, hashing
+Javascript: D3: event handlers, svg's, tool tips
+html & css: forms
+Procedure followed:
+1 : Data in csv format is obtained from: 
 https://www.kaggle.com/datasnaek/youtube-new#USvideos.csv
 https://www.kaggle.com/datasnaek/youtube-new#CAvideos.csv
+
+2. Data is inspected and prepared using panda dataframes
+
+3. Dataframes are converted to dictionaries and fed to mongoDB 
+
+4. Create a flask app that accesses our data from mongodb and later passes this data to a javascript program upon request. 
+
+Using Javascript: 
+
+Receive data from our flask app and do some analysis by grouping videos by category and generating a summary of the categories such as:
+- average likes 
+- average comments
+- average dislikes
+- average views 
+
+6. Create an svg and create a bubble plot that holds some info of the category summmaries. Data visualizations are available for CA and US Trending youtube videos.
+
+7. Use the Naive Bayes model to predict the category a video belongs to given the Title of the video. 
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import requests
 import pymongo
 from bson import Binary, Code
 from bson.json_util import dumps
-import numpy as np
 import pandas as pd
-import collections
+#import collections
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
@@ -45,42 +44,55 @@ def index():
 def predict():
    print("we are in:/ predict")
    if request.method == 'POST':
+
+    #load data
       USvids = pd.read_csv("dataCSV/USvideos.csv", header=0)
       USvids = USvids[['title','category_id']]
       Categories_JSON = pd.read_json("dataCSV/US_category_id.JSON")
+    #create a dictionary
       CategoryDict = [{'id': item['id'], 'title': item['snippet']['title']} for item in Categories_JSON['items']]
       
-
+    
       vector = CountVectorizer()
       counts = vector.fit_transform(USvids['title'].values)
 
       NB_Model = MultinomialNB()
       targets = USvids['category_id'].values
+
+      #fit naive_bayes model with the titles and categories
       NB_Model.fit(counts,targets)
 
+    #split the data
       X= counts
       y= targets
       X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .1)
-
+    #train the model
       NBtest = MultinomialNB().fit(X_train, y_train)
+    #test the model
       nb_predictions = NBtest.predict(X_test)
       acc_nb = NBtest.score(X_test, y_test)
+
       print('The Naive Bayes Algorithm scored an accuracy of', acc_nb)
       print("this is the form response:")
+      #take a title from user input
       print(request.form["title"])
       titleList = []
       titleList.append(request.form["title"])
       print("this is the titleList")
       print(titleList)
       Titles_counts = vector.transform(titleList)
+
+      #model makes prediction
       Predict = NB_Model.predict(Titles_counts)
 
+    
       CategoryNamesList = []
       for Category_ID in Predict:
         MatchingCategories = [x for x in CategoryDict if x["id"] == str(Category_ID)]
         if MatchingCategories:
           CategoryNamesList.append(MatchingCategories[0]["title"])
-
+    
+        #obtain category prediction
         
    
 
